7f31cd9c0f9731be3df798063d3020ca45fcb1e7
blackheart7565 <give.me.heart8844@gmail.com>
2024-07-28 16:40:51 +0300

    feat: start server

---
+++ server/index.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+
+import http from "http";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import path from "path";
+import bodyParser from "body-parser";
+import Fingerprint from "./node_modules/express-fingerprint/lib/index";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(cors({
+	origin: process.env.BASE_URL_CLIENT as string,
+	credentials: true,
+}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(Fingerprint({
+	parameters: [
+		//@ts-ignore
+		Fingerprint.useragent,
+		//@ts-ignore
+		Fingerprint.acceptHeaders,
+	]
+}));
+app.use(fileUpload({}));
+
+const PORT: number = parseInt(process.env.PORT as string);
+
+server.listen(PORT, () => {
+	console.log(`Server listen on http://localhost:${PORT}`);
+});
+
+
+
+
+
