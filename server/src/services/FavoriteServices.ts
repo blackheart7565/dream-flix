c9f1cccf51f240b8506fd370a15f9c8fa6a2f0fb
blackheart7565 <give.me.heart8844@gmail.com>
2024-07-28 20:51:59 +0300

    feat: create FavoriteServices class
    - Implement FavoriteServices class for managing user favorites

---
+++ server/src/services/FavoriteServices.ts
@@ -0,0 +1,47 @@
+
+import type { IObjectIdType } from "../types/db.type";
+import type {
+	IGetAllReturn,
+	IFavoriteCreateProps,
+	IFavoriteCreateReturn,
+	IFavoriteDeleteProps,
+	IFavoriteDeleteReturn,
+	IFavoriteDeleteAllProps,
+	IFavoriteDeleteAllReturn,
+} from "../types/favorite.type";
+
+interface IFavoriteServices { }
+
+class FavoriteServices implements IFavoriteServices {
+
+	public async getAll(userId: IObjectIdType): Promise<IGetAllReturn | void> {
+		
+	}
+
+	public async create({
+		userId,
+		mediaId,
+		mediaType,
+		mediaPosterPath,
+		mediaTitle,
+		mediaRating,
+		mediaReleaseDate
+	}: IFavoriteCreateProps): Promise<IFavoriteCreateReturn | void> {
+
+	}
+
+	public async delete({
+		userId,
+		favoriteId
+	}: IFavoriteDeleteProps): Promise<IFavoriteDeleteReturn | void> {
+		
+	}
+
+	public async deleteAll({
+		userId,
+	}: IFavoriteDeleteAllProps): Promise<IFavoriteDeleteAllReturn[] | void> {
+		
+	}
+}
+
+export default new FavoriteServices();
