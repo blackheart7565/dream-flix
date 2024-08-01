8acf67aa751d5d75a7196d7bcb44118a50a81483
blackheart7565 <give.me.heart8844@gmail.com>
2024-08-01 22:49:42 +0300

    feat: add style reset
    - Implement a reset of styles to ensure consistent
      appearance across browsers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import { RootRoutes } from "./components/layouts/RootRoutes";
 
+import "./styles/reset.css";
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!)
