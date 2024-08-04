6e831f1b169cb513cb77f69a14ccd80102d8f118
blackheart7565 <give.me.heart8844@gmail.com>
2024-08-04 16:36:37 +0300

    feat: add Header for pages
    - Added a Header component for various pages.

---
+++ client/src/components/elements/Header/Header.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import classNames from "classnames";
+
+import { Logo } from "../Logo/Logo";
+import { Navigation } from "../Navigation/Navigation";
+import { UserDropDown } from "../UserDropDown/UserDropDown";
+import {
+	ButtonTheme,
+	ButtonFavorite,
+	ButtonBurgerMenu,
+} from "../../UI";
+
+import style from "./Header.module.scss";
+
+interface IHeaderProps { }
+
+export const Header: React.FC<IHeaderProps> = (): JSX.Element => {
+	return (
+		<header className={style.header}>
+			<div className={classNames("container", style.headerInner)}>
+				<Logo />
+				<Navigation />
+				<div className={style.btnGroup}>
+					<ButtonFavorite />
+					<ButtonTheme id={"btn-theme"} />
+					<UserDropDown />
+				</div>
+				<ButtonBurgerMenu />
+			</div>
+		</header>
+	);
+};
--- client/src/components/layouts/MainLayout.tsx
+++ client/src/components/layouts/MainLayout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import { Header } from "../elements/Header/Header";
+
 interface IMainLayoutProps { }
 
 export const MainLayout: React.FC<IMainLayoutProps> = (): JSX.Element => {
 	return (
 		<>
+			<Header />
 			<Outlet />
 		</>
 	);
