f8cfac9667be440d166e725539541c49db1de340
blackheart7565 <give.me.heart8844@gmail.com>
2024-08-04 18:42:21 +0300

    chore: change main index styles extension from .css to .scss

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import { RootRoutes } from "./components/layouts/RootRoutes";
 
 import "./styles/reset.css";
 import "./styles/importFonts.css";
-import './styles/index.css';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!)
 	.render(
